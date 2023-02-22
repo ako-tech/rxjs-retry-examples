56dc88844de9b17a028b5a0dbfb70913ef34f330
ako-tech <akotech20@gmail.com>
2023-02-22 11:45:57 +0100

    initial setup

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
+import { tap, timer } from "rxjs";
 import "./style.css";
+
+const source = timer(0, 1000).pipe(
+    tap((count) => {
+        if (count === 3) {
+            throw new Error("La cuenta ha llegado a 3");
+        }
+    }),
+    tap({
+        subscribe: () => console.log("Se produce la subscripcion"),
+        error: () => console.log("Se lanza el error"),
+    })
+);
