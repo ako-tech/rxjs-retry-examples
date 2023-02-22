1f62fc06e33a5ca7535711c4f4901b6ab2628ae7
ako-tech <akotech20@gmail.com>
2023-02-22 12:47:07 +0100

    remove initialOffset

--- src/backoff.retry.ts
+++ src/backoff.retry.ts
@@ -2,15 +2,8 @@ import { Observable, timer } from "rxjs";
 
 type BackoffDelayFn = (error: any, retryCount: number) => Observable<number>;
 
-interface BackoffConfig {
-    initialOffset: number;
-    base: number;
-}
-
-export function exponentialBackOff({
-    initialOffset = 1000,
-    base = 2,
-}: BackoffConfig): BackoffDelayFn {
+export function exponentialBackOff(base: number): BackoffDelayFn {
+    const factorToMs = 1000;
     return (_, retryCount) =>
-        timer(initialOffset * Math.pow(base, retryCount - 1));
+        timer(Math.pow(base, retryCount - 1) * factorToMs);
 }
